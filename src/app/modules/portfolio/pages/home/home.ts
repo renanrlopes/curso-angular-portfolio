e8f9ddcf05e06112b2d8903b44c93d5912699067
Renan Lopes <rrloppes7@gmail.com>
2026-01-12 12:52:56 -0300

    ajustando projetos

--- src/app/modules/portfolio/pages/home/home.ts
+++ src/app/modules/portfolio/pages/home/home.ts
@@ -4,10 +4,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Knowledge } from '../../components/knowledge/knowledge';
 import { Experiences } from '../../components/experiences/experiences';
+import { Projects } from '../../components/projects/projects';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Knowledge, Experiences],
+  imports: [Header, Knowledge, Experiences, Projects],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
